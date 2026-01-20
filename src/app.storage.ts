3eff54d74e816abd4f7c5d2120a22030cffa2866
Ansuman <ansumanp@mindfiresolutions.com>
2026-01-20 17:47:44 +0530

    [ADD: installed jiti for eslint --fix

--- src/app.storage.ts
+++ src/app.storage.ts
@@ -1,23 +1,23 @@
-const setLocalStorage = (data: any[]): void => {
-    const localStorageString: string = JSON.stringify(data);
+// const setLocalStorage = (data: any[]): void => {
+//     const localStorageString: string = JSON.stringify(data);
 
-    localStorage.setItem('portfolioFormData', localStorageString)
-}
+//     localStorage.setItem('portfolioFormData', localStorageString)
+// }
 
- const getLocalStorage = (): any[] => {
+//  const getLocalStorage = (): any[] => {
 
-    const localStorageString: string | null = localStorage.getItem('portfolioFormData')
+//     const localStorageString: string | null = localStorage.getItem('portfolioFormData')
 
-    if (localStorageString) {
-        const localStorageData = JSON.parse(localStorageString)
-        return localStorageData
-    }else {
-        return []
-    }
-}
+//     if (localStorageString) {
+//         const localStorageData = JSON.parse(localStorageString)
+//         return localStorageData
+//     }else {
+//         return []
+//     }
+// }
 
-const clearLocalStorage = (): void => {
-    localStorage.clear()
-}
+// const clearLocalStorage = (): void => {
+//     localStorage.clear()
+// }
 
-export {setLocalStorage, getLocalStorage, clearLocalStorage}
+// export {setLocalStorage, getLocalStorage, clearLocalStorage}
