b0c3255df377fb2f5d3c0b572d3ae0743d77fc86
Ansuman <ansumanp@mindfiresolutions.com>
2026-01-27 15:32:26 +0530

    chore: intall commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
+    {
+    ignores: ['./commitlint.config.cjs'],
+  },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -20,4 +23,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
